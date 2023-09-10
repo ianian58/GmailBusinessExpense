62f0f2845dd5dc05d9069975024478b69b05d7fc
ianian58 <garciian58@icloud.com>
2023-09-10 12:56:33 -0400

    Changed from Tesseract to Google Vision.

--- ocr_handling.py
+++ ocr_handling.py
@@ -1,10 +1,26 @@
+import os
+from google.cloud import vision
 from PIL import Image
-import pytesseract
 import io
+
+# Set up Google Cloud Vision credentials
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/ian/Downloads/virtual-bonito-398414-0712f06c51d6.json"
 
 def get_text_from_image(image_data):
     """
-    Extract and return text from a given image data using Tesseract OCR.
+    Uses Google Cloud Vision API to extract text from a given image data.
     """
-    image = Image.open(io.BytesIO(image_data))
-    return pytesseract.image_to_string(image)
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image(content=image_data)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    if texts:
+        return texts[0].description
+    else:
+        return "No text found."
+
+class OCRError(Exception):
+    """Custom exception to handle OCR errors."""
+    pass
